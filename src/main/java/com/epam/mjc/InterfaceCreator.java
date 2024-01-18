e3832a6ac5899669c5f4c16f9f56c1999d69f3d2
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2024-01-19 02:12:25 +0600

    fs

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,7 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -8,35 +10,65 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-interface PredicateCreation{
-
-}
-
-
 public class InterfaceCreator {
 
-    public Predicate<List<String>> isValuesStartWithUpperCase = x ->  {
-        for(var e : x){
-            if(!Character.isUpperCase(e.charAt(0)))
-                return false;
-        }
+    public Predicate<List<String>> isValuesStartWithUpperCase(){
+        return x -> {
+            for(var e : x){
+                if(!Character.isUpperCase(e.charAt(0)))
+                    return false;
+            }
 
-        return true;
-    };
+            return true;
+        };
+    }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            int counter = x.size() - 1;
+            int check = x.size() - 1;
+            for (int i = 0; i < x.size(); i++){
+                if (x.get(i) % 2 == 0) {
+                    counter += 1;
+                    x.add(x.get(i));
+                }
+                if(i == check)
+                    break;
+            }
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> {
+            List<String> list = new ArrayList<>();
+            for(var e : values){
+                String[] arr = e.split(" ");
+                String firstWord = arr[0];
+                String lastWord = arr[arr.length-1];
+                char dotCheck = lastWord.charAt(lastWord.length()-1);
+                if(Character.isUpperCase(firstWord.charAt(0)) && dotCheck=='.' && arr.length>3)
+                    list.add(e);
+            }
+            return list;
+        };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            Map<String, Integer> map = new HashMap<>();
+            for(var e : x){
+                map.put(e, e.length());
+            }
+            return map;
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            List<Integer> list = new ArrayList<>();
+            list.addAll(list1);
+            list.addAll(list2);
+            return list;
+        };
     }
 }
